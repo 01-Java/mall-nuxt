3479741bb81c64e29cdc7a9b20ce940ea41b2724
ruan-cat <1219043956@qq.com>
2025-02-17 01:17:56 +0800

    ✨ feat: 改造store存储的语法。

---
+++ nuxt/app/composables/states/cart.ts
@@ -0,0 +1,46 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+
+interface CartItem {
+	id: string;
+	skuId: string;
+	name: string;
+	picture: string | string[];
+	price: number;
+	count: number;
+	attrsText: string;
+}
+
+export const useCartState = defineStore("cart", () => {
+	const cartList = ref<CartItem[]>([]);
+
+	const allCount = computed(() => {
+		return cartList.value.reduce((sum, item) => sum + item.count, 0);
+	});
+
+	const allPrice = computed(() => {
+		return cartList.value.reduce((sum, item) => sum + item.count * item.price, 0);
+	});
+
+	function addCart(item: CartItem) {
+		const goods = cartList.value.find((good) => good.skuId === item.skuId);
+		if (goods) {
+			goods.count++;
+		} else {
+			cartList.value.push(item);
+		}
+	}
+
+	function deleteCart(skuId: string) {
+		const idx = cartList.value.findIndex((item) => item.skuId === skuId);
+		cartList.value.splice(idx, 1);
+	}
+
+	return {
+		cartList,
+		allCount,
+		allPrice,
+		addCart,
+		deleteCart,
+	};
+});
---
+++ nuxt/app/composables/states/user.ts
@@ -0,0 +1,57 @@
+import { defineStore } from "pinia";
+
+interface Profile {
+	id: string;
+	account: string;
+	mobile: string;
+	token: string;
+	avatar: string;
+	nickname: string;
+	gender: string;
+	birthday: string;
+	cityCode: string;
+	provinceCode: string;
+	profession: string;
+}
+
+export const useUserState = defineStore("user", () => {
+	const profile = ref<Profile>({
+		id: "",
+		account: "",
+		mobile: "",
+		token: "",
+		avatar: "",
+		nickname: "",
+		gender: "",
+		birthday: "",
+		cityCode: "",
+		provinceCode: "",
+		profession: "",
+	});
+
+	function setUser(payload: Profile) {
+		profile.value = payload;
+	}
+
+	function logout() {
+		profile.value = {
+			id: "",
+			account: "",
+			mobile: "",
+			token: "",
+			avatar: "",
+			nickname: "",
+			gender: "",
+			birthday: "",
+			cityCode: "",
+			provinceCode: "",
+			profession: "",
+		};
+	}
+
+	return {
+		profile,
+		setUser,
+		logout,
+	};
+});
