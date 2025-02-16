a415453c8abca1e14644edb0212c7e4f9e9cf548
ruan-cat <1219043956@qq.com>
2025-02-16 13:54:43 +0800

    🎉 init: 开始更改。开始从nuxt配置更改。

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,14 +1,13 @@
-import { pwa } from './app/config/pwa'
-import { appDescription } from './app/constants/index'
+import { pwa } from "./app/config/pwa";
+import { appDescription } from "./app/constants/index";
 
 export default defineNuxtConfig({
   modules: [
-    '@vueuse/nuxt',
-    '@unocss/nuxt',
-    '@pinia/nuxt',
-    '@nuxtjs/color-mode',
-    '@vite-pwa/nuxt',
-    '@nuxt/eslint',
+    "@vueuse/nuxt",
+    "@pinia/nuxt",
+    "@unocss/nuxt",
+    "@vite-pwa/nuxt",
+    "@element-plus/nuxt",
   ],
 
   devtools: {
@@ -17,28 +16,52 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      viewport: 'width=device-width,initial-scale=1',
+      viewport: "width=device-width,initial-scale=1",
+      title: "Nuxt Mall",
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: "icon", href: "/favicon.ico", sizes: "any" },
+        { rel: "icon", type: "image/svg+xml", href: "/vite.svg" },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
       ],
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: appDescription },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-        { name: 'theme-color', media: '(prefers-color-scheme: light)', content: 'white' },
-        { name: 'theme-color', media: '(prefers-color-scheme: dark)', content: '#222222' },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: appDescription },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
+        {
+          name: "theme-color",
+          media: "(prefers-color-scheme: light)",
+          content: "white",
+        },
+        {
+          name: "theme-color",
+          media: "(prefers-color-scheme: dark)",
+          content: "#222222",
+        },
       ],
     },
   },
 
   css: [
-    '@unocss/reset/tailwind.css',
+    "@unocss/reset/tailwind.css",
+    "~/styles/main.scss",
+    "//at.alicdn.com/t/font_2143783_iq6z4ey5vu.css",
   ],
 
   colorMode: {
-    classSuffix: '',
+    classSuffix: "",
+  },
+
+  runtimeConfig: {
+    apiSecret: "",
+    public: {
+      apiBase: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
+    },
+  },
+  devServer: {
+    port: 8080,
   },
 
   future: {
@@ -46,25 +69,23 @@ export default defineNuxtConfig({
   },
 
   experimental: {
-    // when using generate, payload js assets included in sw precache manifest
-    // but missing on offline, disabling extraction it until fixed
     payloadExtraction: false,
     renderJsonPayloads: true,
     typedPages: true,
   },
 
-  compatibilityDate: '2024-08-14',
+  compatibilityDate: "2024-08-14",
 
   nitro: {
     esbuild: {
       options: {
-        target: 'esnext',
+        target: "esnext",
       },
     },
     prerender: {
       crawlLinks: false,
-      routes: ['/'],
-      ignore: ['/hi'],
+      routes: ["/"],
+      ignore: ["/hi"],
     },
   },
 
@@ -78,4 +99,4 @@ export default defineNuxtConfig({
   },
 
   pwa,
-})
+});
